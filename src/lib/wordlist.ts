036d0aa14ed57b592512c757ca5a9fb072d139de
Oliver Bramhill <oliverbramhill@Olivers-MacBook-Pro.local>
2026-06-22 09:33:24 +0100

    feat: add pirate/nautical word list for room code generation

---
+++ src/lib/wordlist.ts
@@ -0,0 +1,22 @@
+export const ADJECTIVES = [
+  'jolly', 'cursed', 'salty', 'black', 'crimson', 'ancient', 'fearless',
+  'sunken', 'stormy', 'golden', 'silent', 'roaring', 'ghostly', 'iron',
+  'wild', 'brave', 'dead', 'silver', 'dread', 'mighty', 'wicked', 'lost',
+  'royal', 'raging', 'hollow', 'broken', 'dark', 'blazing', 'frozen', 'swift',
+];
+
+export const NOUNS = [
+  'anchor', 'kraken', 'cannon', 'compass', 'cutlass', 'galleon', 'lantern',
+  'plank', 'reef', 'skull', 'tide', 'voyage', 'barnacle', 'bilge', 'cove',
+  'davey', 'flagship', 'hull', 'jollyroger', 'mast', 'mariner', 'orca',
+  'porthole', 'quarterdeck', 'rigging', 'serpent', 'starboard', 'squall',
+  'tempest', 'whirlpool',
+];
+
+export const OBJECTS = [
+  'atlas', 'beacon', 'brig', 'buccaneer', 'chart', 'chest', 'crow',
+  'cutlass', 'dagger', 'doubloon', 'flint', 'grog', 'helm', 'horizon',
+  'isle', 'knot', 'loot', 'mainsail', 'mizzen', 'parrot', 'pearl',
+  'pistol', 'powder', 'prize', 'rum', 'sextant', 'shackle', 'siren',
+  'spyglass', 'trident',
+];
